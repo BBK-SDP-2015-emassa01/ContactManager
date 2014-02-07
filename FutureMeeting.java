39522326536c7ad3eec8825a3327e38bd5c07054
Esha Massand <ubemas001@mail.bbk.ac.uk>
2014-02-07 01:49:18 +0000

    First commit of the interfaces provided by the assignment

---
+++ FutureMeeting.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Esha
+ */
+
+/**
+* A meeting to be held in the future
+*/
+public interface FutureMeeting extends Meeting{
+// No methods here, this is just a naming interface (i.e. only necessary for type checking and/or downcasting)
+}
+    
+
---
+++ PastMeeting.java
@@ -0,0 +1,29 @@
+package ContactManager;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Esha
+ */
+/**
+* A meeting that was held in the past.
+*
+* It includes your notes about what happened and what was agreed.
+*/
+public interface PastMeeting extends Meeting {
+/**
+* Returns the notes from the meeting.
+*
+* If there are no notes, the empty string is returned.
+*
+* @return the notes from the meeting.
+*/
+String getNotes();
+}
+    
+
