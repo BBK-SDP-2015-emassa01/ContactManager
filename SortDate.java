19be4812ac684186ce65c32a16fa2844bb432405
Esha Massand <ubemas001@mail.bbk.ac.uk>
2014-03-04 23:17:34 +0000

    formatting

--- SortDate.java
+++ SortDate.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package ContactManager;
 
 import java.io.Serializable;
@@ -15,18 +14,15 @@ import java.util.Comparator;
  */
 public class SortDate implements Comparator<Meeting> {
 
-
     @Override
     public int compare(Meeting o1, Meeting o2) {
-        if (!(o1 instanceof Meeting) || !(o2 instanceof Meeting)){
+        if (!(o1 instanceof Meeting) || !(o2 instanceof Meeting)) {
             throw new ClassCastException("Cannot cast this object to class 'Meeting' to carry out comparison.");
         }
-        if( o1.getDate() == null || o2.getDate() ==null){
-        throw new NullPointerException("You entered a null Meeting to compare.");
+        if (o1.getDate() == null || o2.getDate() == null) {
+            throw new NullPointerException("You entered a null Meeting to compare.");
         }
         return o1.getDate().compareTo(o2.getDate());
     }
-    
 
-    
 }
